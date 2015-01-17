5204208359a5bf7f13f983b0270e4ee157598112
estebanjara <estebandotcom@hotmail.com>
2015-01-16 21:32:11 -0500

    Con abecedario

--- cinco_mil.rb
+++ cinco_mil.rb
@@ -1,6 +1,11 @@
 archivo= File.open "cincomil.txt","w"
-for i in 0..7500
+[*('a'..'z'), *('0'..'5000')].each do |i|
 	print i
 	archivo.write (i)
 	archivo.write ("\n")
+end
+
+3.times do 
+	puts "hola"
+
 end
